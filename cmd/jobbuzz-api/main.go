4e265353b7e139d9a6259d185117fa5ddeb2e43f
Desmond Chin <chin.sy.des@gmail.com>
2022-02-11 16:39:18 +0800

    use pretty print if terminal and json if not

--- cmd/jobbuzz-api/main.go
+++ cmd/jobbuzz-api/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"io"
 	"os"
+	"time"
 
 	"github.com/b-open/jobbuzz/internal/config"
 	"github.com/b-open/jobbuzz/pkg/controller"
 	"github.com/b-open/jobbuzz/pkg/service"
 	"github.com/gin-contrib/logger"
 	"github.com/gin-gonic/gin"
+	"github.com/mattn/go-isatty"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -18,12 +21,15 @@ func main() {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	}
 
-	log.Logger = log.Output(
-		zerolog.ConsoleWriter{
-			Out:     os.Stderr,
-			NoColor: false,
-		},
-	)
+	isTerm := isatty.IsTerminal(os.Stdout.Fd())
+	if isTerm {
+		log.Logger = log.Output(
+			zerolog.ConsoleWriter{
+				Out:     os.Stderr,
+				NoColor: false,
+			},
+		)
+	}
 
 	configuration, err := config.LoadConfig("../../")
 	if err != nil {
@@ -40,7 +46,29 @@ func main() {
 
 	r := gin.New()
 
-	r.Use(logger.SetLogger())
+	r.Use(logger.SetLogger(
+		logger.WithLogger(func(c *gin.Context, out io.Writer, latency time.Duration) zerolog.Logger {
+			logger := zerolog.New(out)
+			if isTerm {
+				logger = logger.Output(
+					zerolog.ConsoleWriter{
+						Out:     out,
+						NoColor: false,
+					},
+				)
+			}
+			logger = logger.With().
+				Timestamp().
+				Int("status", c.Writer.Status()).
+				Str("method", c.Request.Method).
+				Str("path", c.Request.URL.Path).
+				Str("ip", c.ClientIP()).
+				Dur("latency", latency).
+				Str("user_agent", c.Request.UserAgent()).
+				Logger()
+			return logger
+		}),
+	))
 	r.Use(gin.Recovery())
 
 	r.GET("/api/jobs", controller.GetJobs)
