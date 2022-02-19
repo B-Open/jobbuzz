1e81045f04600c87f3506cd3d7ab1569197ec7d2
Desmond Chin <chin.sy.des@gmail.com>
2022-02-19 11:40:37 +0800

    move handler funcs to controller package

--- cmd/jobbuzz-api/main.go
+++ cmd/jobbuzz-api/main.go
@@ -3,35 +3,11 @@ package main
 import (
 	"log"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/b-open/jobbuzz/graph"
-	"github.com/b-open/jobbuzz/graph/generated"
 	"github.com/b-open/jobbuzz/internal/config"
 	"github.com/b-open/jobbuzz/pkg/controller"
 	"github.com/b-open/jobbuzz/pkg/service"
 	"github.com/gin-gonic/gin"
 )
-
-// Defining the Graphql handler
-func graphqlHandler(service service.Servicer) gin.HandlerFunc {
-	// NewExecutableSchema and Config are in the generated.go file
-	// Resolver is in the resolver.go file
-	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{Service: service}}))
-
-	return func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
-	}
-}
-
-// Defining the Playground handler
-func playgroundHandler() gin.HandlerFunc {
-	h := playground.Handler("GraphQL", "/graphql")
-
-	return func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
-	}
-}
 
 func main() {
 	configuration, err := config.LoadConfig("../../")
@@ -47,16 +23,16 @@ func main() {
 	}
 
 	service := service.Service{DB: db}
-	controller := controller.Controller{Service: &service}
+	c := controller.Controller{Service: &service}
 
 	r := gin.Default()
 
 	if gin.IsDebugging() {
-	    r.GET("/", playgroundHandler())
+		r.GET("/", controller.PlaygroundHandler())
 	}
-	r.POST("/graphql", graphqlHandler(&service))
+	r.POST("/graphql", controller.GraphqlHandler(&service))
 
 	apiV1 := r.Group("/api/v1")
-	apiV1.GET("/job", controller.GetJobs)
+	apiV1.GET("/job", c.GetJobs)
 	r.Run()
 }
